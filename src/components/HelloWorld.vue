fa4badd099f6bbebbb8e57a7cc24f4887a01c21d
manbow1206 <tyasunaga@iyell.jp>
2021-05-22 11:49:44 +0900

    v-if for Conditional rendering

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,16 @@
-<template>
-  <div v-bind:style="styleObject"></div>
+<template v-if="OK">
+  <div v-if="type === 'A'">
+    A
+  </div>
+  <div v-else-if="type === 'B'">
+    B
+  </div>
+  <div v-else-if="type === 'C'">
+    C
+  </div>
+  <div v-else>
+    Not A/B/C
+  </div>
 </template>
 
 <script>
