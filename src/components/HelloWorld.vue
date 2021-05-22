73c3c863cd37e5f968da9cadf264c62daf882994
manbow1206 <tyasunaga@iyell.jp>
2021-05-22 12:14:42 +0900

    v-show for Conditional rendering

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,5 @@
-<template v-if="OK">
-  <div v-if="type === 'A'">
-    A
-  </div>
-  <div v-else-if="type === 'B'">
-    B
-  </div>
-  <div v-else-if="type === 'C'">
-    C
-  </div>
-  <div v-else>
-    Not A/B/C
-  </div>
+<template>
+  <div v-show="OK">Hello!</div>
 </template>
 
 <script>
