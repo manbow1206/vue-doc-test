177855ea27515a82733b59c820850855b446bd8e
manbow1206 <tyasunaga@iyell.jp>
2021-05-24 08:43:53 +0900

    Prop Validation for Props

--- src/components/Lab.vue
+++ src/components/Lab.vue
@@ -8,14 +8,43 @@
 <script>
 export default {
     props: {
-        title: String,
-        likes: Number,
-        isPublished: Boolean,
-        commentIds: Array,
-        author: Object,
-        callback: Function,
-        contactsPromise: Promise
-    },
+        // 基本的な型の検査 (`null` と `undefined` は全てのバリデーションにパスします)
+        propA: Number,
+
+        // 複数の型の許容
+        propB: [String, Number],
+
+        // 文字列型を必須で要求する
+        propC: {
+          type: String,
+          required: true
+        },
+
+        // デフォルト値つきの数値型
+        propD: {
+          type: Number,
+          default: 100
+        },
+
+        // デフォルト値つきのオブジェクト型
+        propE: {
+          type: Object,
+          // オブジェクトもしくは配列のデフォルト値は
+          // 必ずそれを生み出すための関数を返す必要があります。
+          default: function () {
+            return { message: 'hello' }
+          }
+        },
+
+        // カスタマイズしたバリデーション関数
+        propF: {
+          validator: function (value) {
+            // プロパティの値は、必ずいずれかの文字列でなければならない
+            return ['success', 'warning', 'danger'].indexOf(value) !== -1
+          }
+        
+        }
+      },
     data(){
         return {
             // title: "Header",
