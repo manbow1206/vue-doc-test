969a39d3a2c6e2e48bc1ba44a3638e34704ce5f1
manbow1206 <tyasunaga@iyell.jp>
2021-05-26 22:37:21 +0900

    Scoped Slots Shorthand for Slots

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <template>
-    <Lab v-slot="slotProps">
-        {{ slotProps.user.lastName }}
+    <Lab>
+      <template #header>
+        <h1>Here might be a page title</h1>
+      </template>
+    
+      <p>A paragraph for the main content.</p>
+      <p>And another one.</p>
+    
+      <template #footer>
+        <p>Here's some contact info</p>
+      </template>
     </Lab>
 </template>
 <script>
--- src/components/Lab.vue
+++ src/components/Lab.vue
@@ -1,11 +1,16 @@
 <template>
-  <div class="container">
-    <span>
-      <slot v-bind:user="user">
-        {{ user.lastName }}
-      </slot>
-    </span>
-  </div>
+<div class="container">
+  <header>
+    <h1>Here might be a page title</h1>
+  </header>
+  <main>
+    <p>A paragraph for the main content.</p>
+    <p>And another one.</p>
+  </main>
+  <footer>
+    <p>Here's some contact info</p>
+  </footer>
+</div>
 </template>
 
 <script>
