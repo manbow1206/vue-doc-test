cfa870b44d8519e643ed7bbdd69baa81ce079af4
manbow1206 <tyasunaga@iyell.jp>
2021-05-26 21:11:41 +0900

    Basic for Events Handling

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
-  <Lab prop-data="Vue.js !"></Lab>
+  <div id="blog-posts-events-demo">
+  <div :style="{ fontSize: postFontSize + 'em' }">
+    <Lab
+      v-for="post in posts"
+      v-bind:key="post.id"
+      v-bind:post="post"
+      v-on:enlarge-text="postFontSize += 0.1"
+    ></Lab>
+  </div>
+</div>
 </template>
 
 <script>
 import Lab from './components/Lab.vue'
 
 export default {
-   data: {
-    checkedNames: []
+  data: {
+    postFontSize: 1
   },
   components: {
     Lab
--- src/components/Lab.vue
+++ src/components/Lab.vue
@@ -1,54 +1,21 @@
 <template>
-    <div>
-        <h1>{{ propData }}</h1>
-        <p>{{ text }}</p>
-    </div>
 </template>
 
 <script>
 export default {
-    props: {
-        // 基本的な型の検査 (`null` と `undefined` は全てのバリデーションにパスします)
-        propA: Number,
-
-        // 複数の型の許容
-        propB: [String, Number],
-
-        // 文字列型を必須で要求する
-        propC: {
-          type: String,
-          required: true
-        },
-
-        // デフォルト値つきの数値型
-        propD: {
-          type: Number,
-          default: 100
-        },
-
-        // デフォルト値つきのオブジェクト型
-        propE: {
-          type: Object,
-          // オブジェクトもしくは配列のデフォルト値は
-          // 必ずそれを生み出すための関数を返す必要があります。
-          default: function () {
-            return { message: 'hello' }
-          }
-        },
-
-        // カスタマイズしたバリデーション関数
-        propF: {
-          validator: function (value) {
-            // プロパティの値は、必ずいずれかの文字列でなければならない
-            return ['success', 'warning', 'danger'].indexOf(value) !== -1
-          }
-        
-        }
-      },
+  props: ['post'],
+  template: `
+    <div class="blog-post">
+      <h3>{{ post.title }}</h3>
+      <button v-on:click="$emit('enlarge-text')">
+        Enlarge text
+      </button>
+      <div v-html="post.content"></div>
+    </div>
+  `,
     data(){
         return {
-            // title: "Header",
-            text: "Hello Vue.js!"
+          postFontSize: 1
         }
     }
 }
