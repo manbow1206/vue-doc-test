fd57a4e2a7ebcec7c267e91ae6a4954be059de21
manbow1206 <tyasunaga@iyell.jp>
2021-05-26 22:16:01 +0900

    Slot Content for Slots

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,18 @@
 <template>
-  <div id="blog-posts-events-demo">
-  <div :style="{ fontSize: postFontSize + 'em' }">
-    <Lab
-      v-for="post in posts"
-      v-bind:key="post.id"
-      v-bind:post="post"
-      v-on:enlarge-text="postFontSize += 0.1"
-    ></Lab>
+  <div class="home">
+    <Lab>
+      <template v-slot>Child Components</template>
+    </Lab>
+    <Lab>
+      <template v-slot:add>渋谷</template>
+    </Lab>
   </div>
-</div>
 </template>
-
 <script>
 import Lab from './components/Lab.vue'
-
 export default {
-  data: {
-    postFontSize: 1
-  },
   components: {
     Lab
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/components/Lab.vue
+++ src/components/Lab.vue
@@ -1,34 +1,8 @@
 <template>
+  <div class="lab">
+    <p>name:<slot>Parent Components</slot></p>
+    <!-- <p>address:<slot name="add">Osaki</slot></p> -->
+  </div>
 </template>
-
-<script>
-export default {
-  props: ['post'],
-  template: `
-    <div class="blog-post">
-      <h3>{{ post.title }}</h3>
-      <button v-on:click="$emit('enlarge-text')">
-        Enlarge text
-      </button>
-      <div v-html="post.content"></div>
-    </div>
-  `,
-    data(){
-        return {
-          postFontSize: 1
-        }
-    }
-}
-</script>
-
-<style scoped>
-div{
-    border: 2px solid teal
-}
-h1{
-    color: teal
-}
-p{
-    color:rgb(27, 77, 77)
-}
+<style>
 </style>
