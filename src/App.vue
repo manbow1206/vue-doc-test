c3018cc679d352d52d8962cf40d1c9a0d97db520
manbow1206 <tyasunaga@iyell.jp>
2021-05-26 22:33:12 +0900

    for Slots

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,7 @@
 <template>
     <Lab>
-      <template v-slot:header>
-        <h1>Here might be a page title</h1>
-      </template>
-
-      <p>A paragraph for the main content.</p>
-      <p>And another one.</p>
-
-      <template v-slot:footer>
-        <p>Here's some contact info</p>
+      <template v-slot:default="slotProps">
+        {{ slotProps.user.lastName }}
       </template>
     </Lab>
 </template>
--- src/components/Lab.vue
+++ src/components/Lab.vue
@@ -1,15 +1,23 @@
 <template>
   <div class="container">
-    <header>
-      <slot name="header"></slot>
-    </header>
-    <main>
-      <slot></slot>
-    </main>
-    <footer>
-      <slot name="footer"></slot>
-    </footer>
+    <span>
+      <slot v-bind:user="user">
+        {{ user.lastName }}
+      </slot>
+    </span>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        lastName: 'テスト'
+      }
+    }
+  }
+}
+</script>
 <style>
 </style>
