b8cb9f541aefd54c8b4793428fbc08c88570013d
manbow1206 <tyasunaga@iyell.jp>
2021-05-28 08:51:45 +0900

    JS Events Fook for Enter/Leave & List Transitions

--- src/components/Lab.vue
+++ src/components/Lab.vue
@@ -1,17 +1,47 @@
 <template>
-<div id="example-2">
-  <button @click="show = !show">Toggle show</button>
-  <transition :duration="{ enter: 500, leave: 800 }" name="bounce">
-    <p v-if="show">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris facilisis enim libero, at lacinia diam fermentum id. Pellentesque habitant morbi tristique senectus et netus.</p>
+<div id="example-4">
+  <button @click="show = !show">
+    Toggle
+  </button>
+  <transition
+    v-on:before-enter="beforeEnter"
+    v-on:enter="enter"
+    v-on:leave="leave"
+    v-bind:css="false"
+  >
+    <p v-if="show">
+      Demo
+    </p>
   </transition>
 </div>
 </template>
 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/velocity/1.2.3/velocity.min.js"></script>
 <script>
 export default {
   data() {
     return {
       show: true
+    }
+  },
+   methods: {
+    beforeEnter: function (el) {
+      el.style.opacity = 0
+      el.style.transformOrigin = 'left'
+    },
+    enter: function (el, done) {
+      Velocity(el, { opacity: 1, fontSize: '1.4em' }, { duration: 300 })
+      Velocity(el, { fontSize: '1em' }, { complete: done })
+    },
+    leave: function (el, done) {
+      Velocity(el, { translateX: '15px', rotateZ: '50deg' }, { duration: 600 })
+      Velocity(el, { rotateZ: '100deg' }, { loop: 2 })
+      Velocity(el, {
+        rotateZ: '45deg',
+        translateY: '30px',
+        translateX: '30px',
+        opacity: 0
+      }, { complete: done })
     }
   }
 }
