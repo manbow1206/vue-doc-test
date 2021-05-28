0f8b7119bcbada01c5afc6ad0f5466fbb03cf4fa
manbow1206 <tyasunaga@iyell.jp>
2021-05-28 09:41:38 +0900

    & List Transitions

--- src/components/Lab.vue
+++ src/components/Lab.vue
@@ -1,19 +1,9 @@
 <template>
-<div id="example-4">
-  <button @click="show = !show">
-    Toggle
+<transition>
+  <button v-bind:key="docState">
+    {{ buttonMessage }}
   </button>
-  <transition
-    v-on:before-enter="beforeEnter"
-    v-on:enter="enter"
-    v-on:leave="leave"
-    v-bind:css="false"
-  >
-    <p v-if="show">
-      Demo
-    </p>
-  </transition>
-</div>
+</transition>
 </template>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/velocity/1.2.3/velocity.min.js"></script>
@@ -24,24 +14,13 @@ export default {
       show: true
     }
   },
-   methods: {
-    beforeEnter: function (el) {
-      el.style.opacity = 0
-      el.style.transformOrigin = 'left'
-    },
-    enter: function (el, done) {
-      Velocity(el, { opacity: 1, fontSize: '1.4em' }, { duration: 300 })
-      Velocity(el, { fontSize: '1em' }, { complete: done })
-    },
-    leave: function (el, done) {
-      Velocity(el, { translateX: '15px', rotateZ: '50deg' }, { duration: 600 })
-      Velocity(el, { rotateZ: '100deg' }, { loop: 2 })
-      Velocity(el, {
-        rotateZ: '45deg',
-        translateY: '30px',
-        translateX: '30px',
-        opacity: 0
-      }, { complete: done })
+  computed: {
+    buttonMessage: function () {
+      switch (this.docState) {
+        case 'saved': return 'Edit'
+        case 'edited': return 'Save'
+        case 'editing': return 'Cancel'
+      }
     }
   }
 }
