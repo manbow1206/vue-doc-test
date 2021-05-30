4085a5b723984212b7f9a6dcc8259ec3465eceb2
manbow1206 <tyasunaga@iyell.jp>
2021-05-30 21:39:03 +0900

    List Transitions for Enter/Learve & List Transitions

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,47 @@
 <template>
-    <Lab>
-      <template #header>
-        <h1>Here might be a page title</h1>
-      </template>
-    
-      <p>A paragraph for the main content.</p>
-      <p>And another one.</p>
-    
-      <template #footer>
-        <p>Here's some contact info</p>
-      </template>
-    </Lab>
+<div id="list-demo">
+  <button v-on:click="add">Add</button>
+  <button v-on:click="remove">Remove</button>
+  <transition-group name="list" tag="p">
+    <span v-for="item in items" v-bind:key="item" class="list-item">
+      {{ item }}
+    </span>
+  </transition-group>
+</div>
 </template>
+
+
 <script>
-import Lab from './components/Lab.vue'
 export default {
-  components: {
-    Lab
+  data() {
+    return {
+      items: [1,2,3,4,5,6,7,8,9],
+      nextNum: 10
+    }
+  },
+  methods: {
+    randomIndex: function () {
+      return Math.floor(Math.random() * this.items.length)
+    },
+    add: function () {
+      this.items.splice(this.randomIndex(), 0, this.nextNum++)
+    },
+    remove: function () {
+      this.items.splice(this.randomIndex(), 1)
+    },
   }
 }
 </script>
+<style>
+.list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.list-enter-active, .list-leave-active {
+  transition: all 1s;
+}
+.list-enter, .list-leave-to /* .list-leave-active for below version 2.1.8 */ {
+  opacity: 0;
+  transform: translateY(30px);
+}
+</style>
